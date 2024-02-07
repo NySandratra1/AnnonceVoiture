bcfa50a51ff1927493792306b8619c29213042f5
NySandratra <122896188+NySandratra1@users.noreply.github.com>
2024-02-07 11:03:04 +0200

    alefa

--- src/main/java/mg/valian/tsiaro/springbootdemo/security/WebConfig.java
+++ src/main/java/mg/valian/tsiaro/springbootdemo/security/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Content-Type", "Authorization");
+                .allowedHeaders("Access-Control-Allow-Origin","Content-Type", "Authorization");
     }
 }
