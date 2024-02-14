901d0cc1bbc3639d32f621acdc4bafc4aec28dd4
NySandratra1 <122896188+NySandratra1@users.noreply.github.com>
2024-02-14 07:40:49 +0300

    k

--- src/main/java/mg/valian/tsiaro/springbootdemo/data/entity/Messagerie.java
+++ src/main/java/mg/valian/tsiaro/springbootdemo/data/entity/Messagerie.java
@@ -1,29 +1,22 @@
 package mg.valian.tsiaro.springbootdemo.data.entity;
 
-import jakarta.persistence.*;
-import java.sql.Date;
-import lombok.*;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.sql.Timestamp;
 
 
-@Document(collection = "messagerie")
+
 public class Messagerie {
 
-    @Id
-    @Field("_id")
+    
     private String idMessagerie;
 
-    @Field("id_envoyeur")
+    
     private int envoyeur;
 
-    @Field("id_recepteur")
+    
     private int recepteur;
     
-    @Field("date_Message")
     private Timestamp dateMessage;
 
-    @Field("message")
     private String message;
 }
